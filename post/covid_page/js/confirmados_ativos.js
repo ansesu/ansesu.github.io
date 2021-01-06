1013979603dc8376a0ab3de207d1c60292b8e69d
andre <sunaharaseiji@gmail.com>
2021-01-06 17:54:45 -0300

    rebuilding site qua jan  6 17:54:45 -03 2021

--- post/covid_page/js/confirmados_ativos.js
+++ post/covid_page/js/confirmados_ativos.js
@@ -1,34 +1,34 @@
 //Margin
-const marginMLine = { top: 10, right: 10, bottom: 25, left: 76 };
+const marginConfirmados = { top: 10, right: 10, bottom: 25, left: 76 };
 
 //Width and Height
-const widthMLine = 600 - marginMLine.left - marginMLine.right;
-const heightMLine = 300 - marginMLine.top - marginMLine.bottom;
+const widthConfirmados = 600 - marginConfirmados.left - marginConfirmados.right;
+const heightConfirmados = 300 - marginConfirmados.top - marginConfirmados.bottom;
 
 //For converting data
-var parseTimeMLine = d3.timeParse("%Y-%m-%d");
+var parseTimeConfirmados = d3.timeParse("%Y-%m-%d");
 
 //Create categorical color scale
-var colorMLine = d3.scaleOrdinal(['#404040', '#bdbdbd']);
+var colorConfirmados = d3.scaleOrdinal(['#404040', '#bdbdbd']);
 
 //Load in the data
 d3.csv("data/confirmados_ativos.csv")
   .then(function(data) {			
 	//Assign a color to each occupancy columns
-    colorMLine.domain(Object.keys(data[0]).slice(1, 3));
+    colorConfirmados.domain(Object.keys(data[0]).slice(1, 3));
 
     //Parse data
     data.forEach(function(d) {
-    	d.Data = parseTimeMLine(d.Data);		      
+    	d.Data = parseTimeConfirmados(d.Data);		      
      	d.Confirmados = parseInt(d.Confirmados);
      	d.Ativos = parseInt(d.Ativos);
     });
 
 	//Get median of dates
-	medianMLine = d3.median(data, function(d) { return d.Data; })
+	medianConfirmados = d3.median(data, function(d) { return d.Data; })
 
 	//Create categories (for each line plot)
-    var categoriesMLine = colorMLine.domain().map(function(name) {
+    var categoriesConfirmados = colorConfirmados.domain().map(function(name) {
     	return {
     		name: name,
         	values: data.map(function(d) {
@@ -54,29 +54,29 @@ d3.csv("data/confirmados_ativos.csv")
     });
 
 	//Create scale functions
-	xScaleMLine = d3.scaleTime()
+	xScaleConfirmados = d3.scaleTime()
           			 	 .domain(d3.extent(data, function(d) { return d.Data; }))
-          			 	 .range([0, widthMLine]);
-	yScaleMLine = d3.scaleLinear()
+          			 	 .range([0, widthConfirmados]);
+	yScaleConfirmados = d3.scaleLinear()
           				 .domain([0,d3.max(data, function(d) { return d.Confirmados; })])
-          				 .range([heightMLine, 0]);	
+          				 .range([heightConfirmados, 0]);	
 
 	//Create SVG element
-	const svgMLine = d3.select('#multilineplot')
+	const svgConfirmados = d3.select('#multilineplot')
                 	   .append('svg')
                 	    .attr("class", "content")
-                	    .attr("viewBox", `0 0 ${widthMLine + marginMLine.left + marginMLine.right} ${heightMLine + marginMLine.top + marginMLine.bottom}`)
+                	    .attr("viewBox", `0 0 ${widthConfirmados + marginConfirmados.left + marginConfirmados.right} ${heightConfirmados + marginConfirmados.top + marginConfirmados.bottom}`)
                 	    .attr("preserveAspectRatio", "xMidYMid meet")
 	    
-	var gMLine = svgMLine.append("g")
-	                      .attr("transform", "translate(" + marginMLine.left + "," + marginMLine.top + ")");
+	var gConfirmados = svgConfirmados.append("g")
+	                      .attr("transform", "translate(" + marginConfirmados.left + "," + marginConfirmados.top + ")");
 
 	//Create legend
-    var legendMLine = gMLine.selectAll('g')
-                            .data(categoriesMLine)
+    var legendConfirmados = gConfirmados.selectAll('g')
+                            .data(categoriesConfirmados)
                             .enter().append('g')
                              .attr('class', 'legend');
-    legendMLine.append('rect')
+    legendConfirmados.append('rect')
                 .attr('x', 10)
                 .attr('y', function(d, i) {
                		return i * 17.3;
@@ -84,9 +84,9 @@ d3.csv("data/confirmados_ativos.csv")
         	      .attr('width', 12)
         	      .attr('height', 12)
                 .style('fill', function(d) {
-               		return colorMLine(d.name);
+               		return colorConfirmados(d.name);
                 });
-    legendMLine.append('text')
+    legendConfirmados.append('text')
                 .attr('x', 25)
                 .attr('y', function(d, i) {
       		         return (i * 17.3)+11;
@@ -96,102 +96,102 @@ d3.csv("data/confirmados_ativos.csv")
       	        });
 
     //Create axis
-	var xAxisMLine = d3.axisBottom(xScaleMLine)
+	var xAxisConfirmados = d3.axisBottom(xScaleConfirmados)
           				   .ticks(7)
           				   .tickFormat(formatAsMonth)
           				   .tickSize(3);
-	var yAxisMLine = d3.axisLeft(yScaleMLine)
+	var yAxisConfirmados = d3.axisLeft(yScaleConfirmados)
 				             .ticks(7)
 				             .tickSize(3);
-	gMLine.append("g")
+	gConfirmados.append("g")
     	   .attr("class", "xAxis")
-    	   .attr("transform", "translate(0," + heightMLine + ")")
-    	   .call(xAxisMLine)
+    	   .attr("transform", "translate(0," + heightConfirmados + ")")
+    	   .call(xAxisConfirmados)
     	   .select(".domain")
     	    .attr("stroke","#252525")
     	    .attr("stroke-width","0");
-	gMLine.append("g")
+	gConfirmados.append("g")
 	       .attr("class", "yAxis")
 	       .attr("transform", "translate(0,0)")
-	       .call(yAxisMLine)
+	       .call(yAxisConfirmados)
 
 	//Create axis label
-    gMLine.append("text")
+    gConfirmados.append("text")
            .attr("class", "axis-title")
            .attr("transform", "rotate(-90)")
            .style("text-anchor", "middle")
            .attr("y",-75)
-           .attr("x",-heightMLine/2)
+           .attr("x",-heightConfirmados/2)
            .attr("dy", ".71em")
            .text("Casos");
-    categoriesMLine1 = categoriesMLine;
+    categoriesConfirmados1 = categoriesConfirmados;
 	//Create each category object
-	var categoryMLine = gMLine.selectAll(".category")
-                              .data(categoriesMLine)
+	var categoryConfirmados = gConfirmados.selectAll(".category")
+                              .data(categoriesConfirmados)
                                .enter().append("g")
                                .attr("class", "category");
 
 	//Create lines
-	var lineMLine = d3.line()
-          			   .x( function(d) { return xScaleMLine(d.Data); })
-          			   .y( function(d) { return yScaleMLine(d.Cases); });
+	var lineConfirmados = d3.line()
+          			   .x( function(d) { return xScaleConfirmados(d.Data); })
+          			   .y( function(d) { return yScaleConfirmados(d.Cases); });
 
-	categoryMLine.append("path")
+	categoryConfirmados.append("path")
       		     .attr("class", "multiline")
       	         .attr("d", function(d) {
-      	         	return lineMLine(d.values);
+      	         	return lineConfirmados(d.values);
       	         })
       	         .style("stroke", function(d) {
-      	         	return colorMLine(d.name);
+      	         	return colorConfirmados(d.name);
       	         });
 
 	// Create tooltip
-    var focusMLine = gMLine.append("g")
+    var focusConfirmados = gConfirmados.append("g")
                            .attr("class", "focus");
 
-    focusMLine.append("path")
+    focusConfirmados.append("path")
               .attr("class", "hover-line")
 
-    var linesMLine = document.getElementsByClassName('line');
-    var focusPerLineMLine = focusMLine.selectAll('.focus-per-line')
-                                      .data(categoriesMLine)
+    var linesConfirmados = document.getElementsByClassName('line');
+    var focusPerLineConfirmados = focusConfirmados.selectAll('.focus-per-line')
+                                      .data(categoriesConfirmados)
                                       .enter().append("g")
                                        .attr("class", "focus-per-line");
 
-    var tooltipTextMLine = gMLine.selectAll('.tooltip-text')
-	                               .data(categoriesMLine)
+    var tooltipTextConfirmados = gConfirmados.selectAll('.tooltip-text')
+	                               .data(categoriesConfirmados)
 	                               .enter().append("text")
 	                                .attr("class", "tooltip-text");	    
 
-    var tooltipTextDataMLine = gMLine.append("text")
+    var tooltipTextDataConfirmados = gConfirmados.append("text")
             						  .attr("class", "tooltip-date");	                                    
-    focusPerLineMLine.append("circle") 
+    focusPerLineConfirmados.append("circle") 
                       .attr("opacity", "0")
                       .attr("r", 3)
                       .style("stroke", function(d) {
-                      	return colorMLine(d.name);
+                      	return colorConfirmados(d.name);
                       });  
 
-    svgMLine.append('rect') 
+    svgConfirmados.append('rect') 
              .attr("class", "overlay")
-             .attr("transform", "translate(" + marginMLine.left + "," + marginMLine.top + ")")
-             .attr('width', widthMLine)
-             .attr('height', heightMLine)
+             .attr("transform", "translate(" + marginConfirmados.left + "," + marginConfirmados.top + ")")
+             .attr('width', widthConfirmados)
+             .attr('height', heightConfirmados)
              .on("mouseover", function() {
-               	focusMLine.style("display", null); 
-               	tooltipTextMLine.style("display", null);
-				tooltipTextDataMLine.style("display", null);
+               	focusConfirmados.style("display", null); 
+               	tooltipTextConfirmados.style("display", null);
+				tooltipTextDataConfirmados.style("display", null);
              })
              .on("mouseout", function() { 			        	
-               	focusMLine.style("display", "none"); 
-               	tooltipTextMLine.style("display", "none");
-				tooltipTextDataMLine.style("display", "none");
+               	focusConfirmados.style("display", "none"); 
+               	tooltipTextConfirmados.style("display", "none");
+				tooltipTextDataConfirmados.style("display", "none");
              })
-             .on('mousemove', event => mousemoveMLine(event));
+             .on('mousemove', event => mousemoveConfirmados(event));
 
-    function mousemoveMLine(event) { // mouse moving over canvas
+    function mousemoveConfirmados(event) { // mouse moving over canvas
         var mouse = d3.pointer(event),
-            x0 = xScaleMLine.invert(mouse[0]),
+            x0 = xScaleConfirmados.invert(mouse[0]),
             bisectData = d3.bisector(function(d) { return d.Data; }).left,
             i = bisectData(data, x0, 1),
             d0 = data[i - 1],
@@ -201,22 +201,22 @@ d3.csv("data/confirmados_ativos.csv")
         xData = dTrue.Data;
         d3.select("#multilineplot .hover-line")
            .attr("d", function() {
-           		var dVar = "M" + xScaleMLine(xData) + "," + heightMLine;
-            	dVar += " " + xScaleMLine(xData) + "," + 0;
+           		var dVar = "M" + xScaleConfirmados(xData) + "," + heightConfirmados;
+            	dVar += " " + xScaleConfirmados(xData) + "," + 0;
            		return dVar;
            });
         d3.select('#multilineplot .tooltip-date')
            .text(formatAsDate(xData))
            .attr("x", 10)
-           .attr("y", heightMLine - 70 - 3*11);
-        tooltipTextMLine
+           .attr("y", heightConfirmados - 70 - 3*11);
+        tooltipTextConfirmados
            .text(function (d) {if (Number.isNaN(data[idx][d.name])) {   
        		        return "";
        		    } else {
        		        return d.name + ": " + formatValue(data[idx][d.name]);
        		    }})
            .attr("x", 10)
-           .attr("y", function (d,i) { return heightMLine - 55.5 - (2-i)*15.5})
+           .attr("y", function (d,i) { return heightConfirmados - 55.5 - (2-i)*15.5})
         d3.selectAll(".focus-per-line")
            .attr("transform", function(d, i) { 
            	   if (Number.isNaN(data[idx][d.name])) {   
@@ -226,7 +226,7 @@ d3.csv("data/confirmados_ativos.csv")
        		   } else {
            	        d3.select(this).select('.focus-per-line circle')
                        .style("opacity", "1")     
-       		        return "translate(" + xScaleMLine(xData) + "," + yScaleMLine(data[idx][d.name]) +")";
+       		        return "translate(" + xScaleConfirmados(xData) + "," + yScaleConfirmados(data[idx][d.name]) +")";
        		   } 
           });
     };
@@ -237,5 +237,5 @@ d3.csv("data/confirmados_ativos.csv")
 // d3.select(this).select('.focus-per-line text')
 // .text(d.name + ": " + data[idx][d.name])
 // .attr("x", )
-// .attr("y", heightMLine - yScaleMLine(data[idx][d.name]) - 12.5 - (2-i)*15.5)
-// .attr("fill", colorMLine(d.name));
+// .attr("y", heightConfirmados - yScaleConfirmados(data[idx][d.name]) - 12.5 - (2-i)*15.5)
+// .attr("fill", colorConfirmados(d.name));
